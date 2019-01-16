4866ddd079dee6d3b7127925bf3b2cb4e6de0661
Kuan-Yu Chen <chenky@brandeis.edu>
2019-01-15 18:03:38 -0800

    Update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Simulating the Spread of Disease and Virus Population Dynamics
 
-This project is a modification/extension of the Problem Set 3 in [this](https://www.edx.org/course/introduction-computational-thinking-data-mitx-6-00-2x-7) fantastic edX MOOC from MIT.
+This project is a modification/extension of the Problem Set 3 in [this](https://www.edx.org/course/introduction-computational-thinking-data-mitx-6-00-2x-7) fantastic edX MOOC from MIT. For more details, see [ps3.md](https://github.com/chenky0401/virus_simulation/blob/master/ps3.md).
 
-For more details, see [ps3.md](https://github.com/chenky0401/virus_simulation/blob/master/ps3.md).
+To run the program, simply enter `python application.py`.
--- application.py
+++ application.py
@@ -12,14 +12,18 @@ def home():
 
 @app.route("/without_drug", methods=['GET', 'POST'])
 def without_drug():
-	# return pylab.plot(simulationWithoutDrug(1, 1000, 0.1, 0.05, 1), label = "SimpleVirus")
 	data = []
-	numViruses = int(request.form.get("numViruses"))
-	maxPop = int(request.form.get("maxPop"))
-	maxBirthProb = float(request.form.get("maxBirthProb"))
-	clearProb = float(request.form.get("clearProb"))
-	numTrials = int(request.form.get("numTrials"))
-	data.extend(simulationWithoutDrug(numViruses, maxPop, maxBirthProb, clearProb, numTrials))
+	try:
+		numViruses = int(request.form.get("numViruses"))
+		maxPop = int(request.form.get("maxPop"))
+		maxBirthProb = float(request.form.get("maxBirthProb"))
+		clearProb = float(request.form.get("clearProb"))
+		numTrials = int(request.form.get("numTrials"))
+		if numViruses < 0 or maxPop < 0 or maxBirthProb < 0 or maxBirthProb > 1 or clearProb < 0 or clearProb > 1 or numTrials < 0:
+				return ""
+		data.extend(simulationWithoutDrug(numViruses, maxPop, maxBirthProb, clearProb, numTrials))
+	except:
+		return ""
 	# return render_template("plot.html")
 	# return render_template("plot.html", data=data)
 	# return redirect("/plot")
@@ -32,7 +36,9 @@ def without_drug():
 def with_drug():
 	if request.method == 'GET':
 		return jsonify(simulationWithDrug(75, 100, .8, 0.1, {"guttagonol": True}, 0.8, 1))
-# resistance 選單
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
+
+# TODO: resistance 選單
